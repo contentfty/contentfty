a9c0c9e67ed9bd1b75e979a5c7e67e7406bcea69
baisheng@gmail.com <baisheng@gmail.com>
2018-08-22 11:16:04 +0800

    rm graphql temp

--- fty-server/src/graphqlcore/resolve.js
+++
@@ -1,7 +0,0 @@
-const { mapSeries } = require('bluebird')
-const { isObject, isArray, toPairs } = require('lodash')
-
-// const read = function (type, id = null) {
-//   return id === null ?
-// }
-
