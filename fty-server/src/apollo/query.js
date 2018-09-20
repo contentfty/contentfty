d8b53b367f8e2f9f084c2f5ffae588cc5aa7445c
baisheng <baisheng@gmail.com>
2018-09-20 10:04:18 +0800

    Query 成组生成[ById, ByIds, One, Many, Total, Pagination]

--- fty-server/src/apollo/query.js
+++ fty-server/src/apollo/query.js
@@ -110,35 +110,36 @@ const buildObjects = async function (spaceId) {
     })
   )
   // 处理返回值
-  const queryCollection = fromPairs(
-    model.map(structure => {
-      return [
-        structure.name + 'Collection',
-        new GraphQLObjectType({
-          name: structure.name + 'Collection',
-          // interfaces: [EntryInterface],
-          fields: () => {
-            return {
-              skip: {type: new GraphQLNonNull(GraphQLInt)},
-              limit: {type: new GraphQLNonNull(GraphQLInt)},
-              total: {type: new GraphQLNonNull(GraphQLInt)},
-              data: {
-                type: new GraphQLList(new GraphQLObjectType({
-                  name: structure.name + 'Data',
-                  fields: () => {
-                    return {...fromPairs(structure.fields.map(field => [field.name, buildField(field)]))}
-                  }
-                }))
-              }
-              // data: new GraphQLList(fromPairs(structure.fields.map(field => [field.name, buildField(field)])))
-              // data: {...fromPairs(structure.fields.map(field => [field.name, buildField(field)]))}
-            }
-          }
-        })
-      ]
-    })
-  )
-  let ObjectTypes = think._.assign(queryTypes, queryCollection)
+  // const queryCollection = fromPairs(
+  //   model.map(structure => {
+  //     return [
+  //       structure.name + 'Collection',
+  //       new GraphQLObjectType({
+  //         name: structure.name + 'Collection',
+  //         // interfaces: [EntryInterface],
+  //         fields: () => {
+  //           return {
+  //             skip: {type: new GraphQLNonNull(GraphQLInt)},
+  //             limit: {type: new GraphQLNonNull(GraphQLInt)},
+  //             total: {type: new GraphQLNonNull(GraphQLInt)},
+  //             data: {
+  //               type: new GraphQLList(new GraphQLObjectType({
+  //                 name: structure.name + 'Data',
+  //                 fields: () => {
+  //                   return {...fromPairs(structure.fields.map(field => [field.name, buildField(field)]))}
+  //                 }
+  //               }))
+  //             }
+  //             // data: new GraphQLList(fromPairs(structure.fields.map(field => [field.name, buildField(field)])))
+  //             // data: {...fromPairs(structure.fields.map(field => [field.name, buildField(field)]))}
+  //           }
+  //         }
+  //       })
+  //     ]
+  //   })
+  // )
+  // let ObjectTypes = think._.assign(queryTypes, queryCollection)
+  let ObjectTypes = think._.assign(queryTypes)
   ObjectTypes.Schema = buildSchemaObject()
   return ObjectTypes
 }
@@ -187,30 +188,23 @@ const buildQuery = async function (ObjectTypes, spaceId) {
   // 构建全部查询条件输入类型
   const filterType = await buildFilters()
   let queryTypeList = []
+  const suffixMethods = [
+    'ById',
+    'ByIds',
+    'One',
+    'Many',
+    'Total',
+    'Pagination'
+  ]
   const query = new GraphQLObjectType({
     name: 'Query',
     fields: () => {
-      return fromPairs(
-        map(ObjectTypes, (value, key) => {
-            const suffixMethods = [
-              'ById',
-              'ByIds',
-              'One',
-              'Many',
-              'Total',
-              'Pagination'
-            ]
-            let ById = []
-            let ByIds = []
-            let One = []
-            let Many = []
-            let Total = []
-            let Pagination = []
-            // let typeTemp = []
-            // for (let method of suffixMethods) {
-            if (think._.includes(key, 'ById')) {
-              ById = [
-                `${think._.camelCase(key)}ById`,
+        const pairs = map(ObjectTypes, (value, key) => {
+            // 如何 1 变成 4
+            const queryMethods = []
+            suffixMethods.forEach((method) => {
+              queryMethods.push([
+                `${think._.camelCase(key)}${method}`,
                 {
                   type: new GraphQLList(value),
                   args: {
@@ -220,77 +214,13 @@ const buildQuery = async function (ObjectTypes, spaceId) {
                   },
                   resolve: (root, {id}) => read(key, id, spaceId)
                 }
-              ]
-            } else {
-              // if (think._.includes(key, 'ByIds')) {
-              //
-              //   ByIds = [
-              //     `${think._.camelCase(key)}ByIds`,
-              //     {
-              //       type: new GraphQLList(value),
-              //       args: {
-              //         id: {
-              //           type: GraphQLID
-              //         }
-              //       },
-              //       resolve: (root, {id}) => read(key, id, spaceId)
-              //     }
-              //   ]
-
-
-              One = [
-                `${think._.camelCase(key)}One`,
-                {
-                  type: new GraphQLList(value),
-                  args: {
-                    id: {
-                      type: GraphQLID
-                    }
-                  },
-                  resolve: (root, {id}) => read(key, id, spaceId)
-                }
-              ]
-              Many = [
-                `${think._.camelCase(key)}Many`,
-                {
-                  type: new GraphQLList(value),
-                  args: {
-                    id: {
-                      type: GraphQLID
-                    }
-                  },
-                  resolve: (root, {id}) => read(key, id, spaceId)
-                }
-              ]
-              Total = [
-                `${think._.camelCase(key)}Total`,
-                {
-                  type: new GraphQLList(value),
-                  args: {
-                    id: {
-                      type: GraphQLID
-                    }
-                  },
-                  resolve: (root, {id}) => read(key, id, spaceId)
-                }
-              ]
-              Pagination = [
-                `${think._.camelCase(key)}Pagination`,
-                {
-                  type: new GraphQLList(value),
-                  args: {
-                    id: {
-                      type: GraphQLID
-                    }
-                  },
-                  resolve: (root, {id}) => read(key, id, spaceId)
-                }
-              ]
-            }
-            return think._.concat(ById, ByIds, One, Many, Total, Pagination)
+              ])
+            })
+            return think._.concat(queryMethods)
           }
         )
-      )
+      // 去除一层数组形成键值对象
+      return think._.fromPairs(think._.flatten(pairs))
     }
   })
   return query
